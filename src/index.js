63ea832c4e5f6e22f530b72a7909142e35a2d9b4
igorp <igorprohorchenko@gmail.com>
2023-08-08 09:55:01 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix || matrix.length === 0) {
+        return [];
+    }
+
+    const flattened = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) {
+            // Even rows: left to right
+            for (let j = 0; j < matrix[i].length; j++) {
+                flattened.push(matrix[i][j]);
+            }
+        } else {
+            // Odd rows: right to left
+            for (let j = matrix[i].length - 1; j >= 0; j--) {
+                flattened.push(matrix[i][j]);
+            }
+        }
+    }
+
+    return flattened;
 }
